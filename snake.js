19ebdd1f8933bac21e7935cc2f35dc3c4938861d
KristenLingwood <kristen.lingwood@gmail.com>
2018-08-19 14:09:56 -0700

    added two player functionality

--- snake.js
+++ snake.js
@@ -1,40 +1,39 @@
-SCALE = 20
-WIDTH = 30
-HEIGHT = 30
-SPEED = 400
+SCALE = 20;
+WIDTH = 30;
+HEIGHT = 30;
+SPEED = 400;
 
 /** One-time setup: find HTML canvas element */
 
-const canvas = document.getElementById("board")
-canvas.setAttribute("height", HEIGHT * SCALE)
-canvas.setAttribute("width", WIDTH * SCALE)
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById('board');
+canvas.setAttribute('height', HEIGHT * SCALE);
+canvas.setAttribute('width', WIDTH * SCALE);
+const ctx = canvas.getContext('2d');
 
-
-/** Point: 
- * defines coordinates on gameboard. Could be for either snake or food. 
-*/
+/** Point:
+ * defines coordinates on gameboard. Could be for either snake or food.
+ */
 
 class Point {
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        this.x = x;
+        this.y = y;
     }
 
     draw(color) {
-        ctx.fillStyle = color
-        ctx.beginPath()
-        ctx.arc(this.x * SCALE, this.y * SCALE, SCALE / 2, 0, Math.PI * 2)
-        ctx.fill()
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(this.x * SCALE, this.y * SCALE, SCALE / 2, 0, Math.PI * 2);
+        ctx.fill();
     }
 
     static newRandom() {
-        const randRange = (low, hi) => low + Math.floor((Math.random() * (hi - low)))
-        return new Point(randRange(1, WIDTH), randRange(1, HEIGHT))
+        const randRange = (low, hi) => low + Math.floor(Math.random() * (hi - low));
+        return new Point(randRange(1, WIDTH), randRange(1, HEIGHT));
     }
 
     isOutOfBound() {
-        return (this.x <= 0 || this.x >= WIDTH || this.y <= 0 || this.y >= HEIGHT)
+        return this.x <= 0 || this.x >= WIDTH || this.y <= 0 || this.y >= HEIGHT;
     }
 }
 
@@ -42,85 +41,101 @@ class Point {
 
 class Pellet {
     constructor(x, y) {
-        this.pt = new Point(x, y)
+        this.pt = new Point(x, y);
+        //console.log(this.pt)
+
     }
 
     static newRandom() {
-        const pt = Point.newRandom()
-        return new Pellet(pt.x, pt.y)
+        const pt = Point.newRandom();
+        return new Pellet(pt.x, pt.y);
     }
 
     draw() {
-        this.pt.draw("green")
+        this.pt.draw('green');
     }
 }
 
 /** Snake */
 
 class Snake {
-    constructor(keymap, start, dir) {
-        this.keymap = keymap    // mapping of keys to directions
-        this.parts = [start]    // list of x-y coordinates of parts
-        this.dir = dir          // dir to move on next move
-        this.growBy = 0         // how many to grow by (goes up after eating)
+    constructor(keymap, start, dir, color = "orange") {
+        this.keymap = keymap; // mapping of keys to directions
+        this.parts = [start]; // list of x-y coordinates of parts
+        this.dir = dir; // dir to move on next move
+        this.growBy = 0; // how many to grow by (goes up after eating)
+        this.color = color
     }
 
+
     draw() {
-        for (const p of this.parts) p.draw("orange")
+        for (const p of this.parts) p.draw(this.color);
     }
 
     contains(pt, arr = this.parts) {
-        return arr.some(me => me.x === pt.x && me.y === pt.y)
+        return arr.some(me => me.x === pt.x && me.y === pt.y);
     }
 
     crashIntoSelf() {
-        const headless = this.parts.slice(1)
-        return (this.contains(this.head(), headless))
+        const headless = this.parts.slice(1);
+        return this.contains(this.head(), headless);
+    }
+
+    crashIntoOtherSnake(otherSnake) {
+        const head = this.head()
+        return otherSnake.parts.some(o => o.x === head.x && o.y === head.y)
     }
 
     crashIntoWall() {
-        console.log(this.parts[0])
-        // console.log(isOutOfBound)
-        return (this.head().isOutOfBound())
+        return this.head().isOutOfBound();
     }
 
     head() {
-        return this.parts[0]
+        return this.parts[0];
     }
 
     //a move adds a new head(first circle) and removes the tail(last circle)
     move() {
-        const { x, y } = this.head()
-        let pt
-        if (this.dir === "left") pt = new Point(x - 1, y)
-        if (this.dir === "right") pt = new Point(x + 1, y)
-        if (this.dir === "up") pt = new Point(x, y - 1)
-        if (this.dir === "down") pt = new Point(x, y + 1)
-        this.parts.unshift(pt)
+        const { x, y } = this.head();
+        let pt;
+
+        if (this.dir === 'left') pt = new Point(x - 1, y);
+        if (this.dir === 'right') pt = new Point(x + 1, y);
+        if (this.dir === 'up') pt = new Point(x, y - 1);
+        if (this.dir === 'down') pt = new Point(x, y + 1);
+        this.parts.unshift(pt);
     }
 
     //snake handles its own moving by calling the game's key handler
     handleKey(key) {
-        console.log("this.snake inside of handlekey", this.snake)
-        if (this.keymap[key] !== undefined) this.changeDir(this.keymap[key])
+        //console.log('this.snake inside of handlekey', this.snake);
+        if (this.keymap[key] !== undefined) this.changeDir(this.keymap[key]);
     }
 
     changeDir(dir) {
-        this.dir = dir
+        if ((this.dir === "left" || this.dir === "right") && (dir === "left" || dir === "right")) {
+            this.dir = this.dir;
+        }
+        else if ((this.dir === "up" || this.dir === "down") && (dir === "up" || dir === "down")) {
+            this.dir = this.dir;
+        }
+        else {
+            this.dir = dir
+        }
     }
 
     grow() {
-        this.growBy += 2
+        this.growBy += 2;
     }
 
     truncate() {
-        if (this.growBy === 0) this.parts.pop()
-        else this.growBy--
+        if (this.growBy === 0) this.parts.pop();
+        else this.growBy--;
     }
 
     eats(food) {
-        const head = this.head()
-        return food.find(f => f.pt.x === head.x && f.pt.y === head.y)
+        const head = this.head();
+        return food.find(f => f.pt.x === head.x && f.pt.y === head.y);
     }
 }
 
@@ -128,70 +143,81 @@ class Snake {
 
 class Game {
     constructor(snakes) {
-        this.snake = snakes
-        this.food = []
-        this.numFood = 3
+        this.snakes = snakes;
+        this.food = [];
+        this.numFood = 3;
 
-        this.interval = null
-        //binds instance of game to the key listener so that when onkey is called "this" references the instance of game, and not window. 
-        this.keyListener = this.onkey.bind(this)
-
+        this.interval = null;
+        //binds instance of game to the key listener so that when onkey is called "this" references the instance of game, and not window.
+        this.keyListener = this.onkey.bind(this);
     }
 
     refillFood() {
         while (this.food.length < this.numFood) {
-            this.food.push(Pellet.newRandom())
+            let newPellet = Pellet.newRandom();
+            if (this.snakes.every(s => !s.contains(newPellet))) {
+                this.food.push(newPellet);
+            }
         }
     }
 
     play() {
-        document.addEventListener("keydown", this.keyListener)
-        this.interval = window.setInterval(this.tick.bind(this), SPEED)
+        document.addEventListener('keydown', this.keyListener);
+        this.interval = window.setInterval(this.tick.bind(this), SPEED);
     }
 
     //calls the snake's key handler
     onkey(e) {
-        this.snake.handleKey(e.key)
-        console.log("this.snake", this.snake)
+        for (let s of this.snakes) {
+            s.handleKey(e.key);
+        }
     }
 
     removeFood(pellet) {
         this.food = this.food.filter(
-            f => f.pt.x !== pellet.pt.x && f.pt.y !== pellet.pt.y)
+            f => f.pt.x !== pellet.pt.x && f.pt.y !== pellet.pt.y
+        );
     }
 
     tick() {
-        console.log("tick")
+        console.log('tick');
 
-        const dead = this.snake.crashIntoSelf() || this.snake.crashIntoWall()
+        const dead = this.snakes.some(s => s.crashIntoSelf() || s.crashIntoWall() || this.snakes.filter(os => os !== s).some(os => s.crashIntoOtherSnake(os)))
 
         if (!dead) {
-            ctx.clearRect(0, 0, SCALE * WIDTH, SCALE * HEIGHT)
+            ctx.clearRect(0, 0, SCALE * WIDTH, SCALE * HEIGHT);
             for (const f of this.food) {
-                f.draw()
+                f.draw();
             }
-            let eaten
-            this.snake.move()
-            this.snake.truncate()
-            this.snake.draw()
-            if (eaten = this.snake.eats(this.food)) {
-                this.removeFood(eaten)
-                this.snake.grow()
+            for (const s of this.snakes) {
+                let eaten
+                s.move();
+                s.truncate();
+                s.draw();
+                if ((eaten = s.eats(this.food))) {
+                    this.removeFood(eaten);
+                    s.grow();
+                }
             }
-            this.refillFood()
+            this.refillFood();
         } else {
-            window.clearInterval(this.interval)
-            window.removeEventListener("keydown", this.keyListener)
+            window.clearInterval(this.interval);
+            window.removeEventListener('keydown', this.keyListener);
         }
     }
 }
 
-
-const snake = new Snake(
-    { ArrowLeft: "left", ArrowRight: "right", ArrowUp: "up", ArrowDown: "down" },
+const snake1 = new Snake(
+    { ArrowLeft: 'left', ArrowRight: 'right', ArrowUp: 'up', ArrowDown: 'down' },
     new Point(20, 20),
-    "right")
+    'right', 'purple'
+);
 
+const snake2 = new Snake(
+    { a: 'left', d: 'right', w: 'up', s: 'down' },
+    new Point(10, 10),
+    'right', 'pink'
+);
 
-const game = new Game(snake)
-game.play()
+const game = new Game([snake1, snake2]);
+game.play();
